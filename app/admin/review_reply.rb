c16d39f9fb616077d6cbb21b76dafc755315b3e5
Raghu Betina <raghu@firstdraft.com>
2016-10-05 06:06:10 +0000

    Whitelist columns in ReviewReply for ActiveAdmin

--- app/admin/review_reply.rb
+++ app/admin/review_reply.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ReviewReply do
 
+ permit_params :reply_content, :review_id, :restaurant_ownership_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
