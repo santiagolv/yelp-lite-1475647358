3d9e4e422d80e129b18aab8da85c0cc62fec91ed
Raghu Betina <raghu@firstdraft.com>
2016-10-05 06:05:15 +0000

    Direct association between restaurants_profiles and restaurant_ownerships

--- app/models/restaurant_ownership.rb
+++ app/models/restaurant_ownership.rb
@@ -1,6 +1,9 @@
 class RestaurantOwnership < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant,
+             :class_name => "RestaurantsProfile"
+
   # Indirect associations
 
   # Validations
--- app/models/restaurants_profile.rb
+++ app/models/restaurants_profile.rb
@@ -1,6 +1,10 @@
 class RestaurantsProfile < ApplicationRecord
   # Direct associations
 
+  has_many   :restaurant_ownerships,
+             :foreign_key => "restaurant_id",
+             :dependent => :destroy
+
   has_many   :reviews,
              :foreign_key => "restaurant_id",
              :dependent => :destroy
