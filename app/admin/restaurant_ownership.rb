84ca67140f16acdd43ce6be8f1303bc11d8d7af0
Raghu Betina <raghu@firstdraft.com>
2016-10-05 06:06:00 +0000

    Whitelist columns in RestaurantOwnership for ActiveAdmin

--- app/admin/restaurant_ownership.rb
+++ app/admin/restaurant_ownership.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RestaurantOwnership do
 
+ permit_params :user_id, :restaurant_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
