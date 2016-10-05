07e13684e3f7d8868e9385cdc35a9cf5327dbed1
Raghu Betina <raghu@firstdraft.com>
2016-10-05 06:05:57 +0000

    Whitelist columns in PhotoReview for ActiveAdmin

--- app/admin/photo_review.rb
+++ app/admin/photo_review.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PhotoReview do
 
+ permit_params :review_id, :photos
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
