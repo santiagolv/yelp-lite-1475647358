192cc9191a69d88cfecc1a09ec872ed110b37498
Raghu Betina <raghu@firstdraft.com>
2016-10-05 06:06:07 +0000

    Whitelist columns in TagCatalogue for ActiveAdmin

--- app/admin/tag_catalogue.rb
+++ app/admin/tag_catalogue.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register TagCatalogue do
 
+ permit_params :tag_content
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
