ab17b2b99a9681fdae9707521f3ede6bf235acf2
Raghu Betina <raghu@firstdraft.com>
2016-10-05 06:05:15 +0000

    Direct association between restaurants_profiles and reviews

--- app/models/restaurants_profile.rb
+++ app/models/restaurants_profile.rb
@@ -1,6 +1,10 @@
 class RestaurantsProfile < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :foreign_key => "restaurant_id",
+             :dependent => :destroy
+
   belongs_to :neighborhood
 
   # Indirect associations
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant,
+             :class_name => "RestaurantsProfile"
+
   # Indirect associations
 
   # Validations
